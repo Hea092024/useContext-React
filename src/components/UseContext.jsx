b95cde608e7f58da1b15b08884c1eee0d2e314b6
Hector Acevedo <HEA092024@gmail.com>
2025-02-20 12:42:35 +0100

    .

--- src/components/UseContext.jsx
+++ src/components/UseContext.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, createContext , useContext} from "react"
 import { AppContext } from "../App"
 export default function UseContext () {
+
+    const name = useContext(AppContext)
     return (
         <>
 
         
-        <h2>Velkommen....</h2>
+        <h2>Velkommen { name }</h2>
         </>
     )
 }
