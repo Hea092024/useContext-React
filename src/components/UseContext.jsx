5e511d92234f50ce6e57c81c893db8d6180d22fc
Hector Acevedo <HEA092024@gmail.com>
2025-02-20 11:34:33 +0100

    .

--- src/components/UseContext.jsx
+++ src/components/UseContext.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect, createContext } from "react"
 export default function UseContext () {
     return (
         <>
+
+        <AppContext.Provider>
         <h2>Hva er useContext..??</h2>
+        </AppContext.Provider>
         </>
     )
 }
